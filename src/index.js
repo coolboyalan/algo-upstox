0e3adae574bdbe4d58aecd765ef80db73c144b04
Deepak Singh <deepaky954000@gmail.com>
2025-08-18 09:03:52 +0530

    Reverted back to 3 minute

--- src/index.js
+++ src/index.js
@@ -224,11 +224,11 @@ cron.schedule("* * * * * *", async () => {
     if (isInMarketRange && second % 10 === 0) {
       const toTime = toKiteISTFormat(istNow);
       const fromTime = toKiteISTFormat(
-        new Date(istNow.getTime() - 5 * 60 * 1000),
+        new Date(istNow.getTime() - 3 * 60 * 1000),
       );
 
       const instrumentToken = dailyAsset.zerodhaToken;
-      const interval = "5minute";
+      const interval = "3minute";
       const apiKey = adminKeys.apiKey;
       const accessToken = adminKeys.token;
 
@@ -337,7 +337,7 @@ cron.schedule("* * * * * *", async () => {
         );
       }
 
-      console.log({istNow,price,direction,signal})
+      console.log({ istNow, price, direction, signal });
 
       for (const key of keys) {
         try {
@@ -533,7 +533,7 @@ cron.schedule("* * * * * *", async () => {
           }
 
           if (second >= 10) continue;
-          if (istMinute % 5 !== 0) continue;
+          if (istMinute % 3 !== 0) continue;
           if (signal === "No Action") {
             continue;
           }
